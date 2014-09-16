86d6e4285bbab1b7aec26331cceb26273a9a8e28
Horst Meyer <horstmail-sourceforge@yahoo.de>
2014-09-16 07:53:39 +0200

    Undocumented Changes

--- classes/help.php
+++ classes/help.php
@@ -2,13 +2,14 @@
 $menu=$_GET[menu];
 $idwert=$_GET[idwert];
 include("../config.php");
+include("dbtools.php");
 include("helpfunc.php");
 include("bootstrapfunc.php");
 include("../sites/views/wp_".$menu."/showtab.inc.php");
 bootstraphead();
 bootstrapbegin($pararray['headline']);
 
-hilfefunc($menu,$idwert);
+hilfefunc($menu,$idwert,$gdbtyp);
 
 bootstrapend();
 ?>
--- classes/helpfunc.php
+++ classes/helpfunc.php
@@ -1,18 +1,10 @@
 <?php
 header("content-type: text/html; charset=utf-8");
 
-function hilfefunc($menu,$idwert) {
+function hilfefunc($menu,$idwert,$gdbtyp) {
   include("../config.php");
   $pfad = "../sites/views/wp_".$menu."/";
   echo "<a class='btn btn-primary' href='showtab.php?menu=".$menu."&idwert=".$idwert."'>zur&uumlck</a> ";
-//  $query="SELECT * FROM tblmenu_liste WHERE fldIndex=".$idwert;
-//  $result = mysql_query($query) or die(mysql_error());
-//  $line = mysql_fetch_array($result); 
-//  if ($line['fldhelplink']<>"") {
-//    echo "<a class='btn btn-primary' href='http://localhost/tools/mediawiki/index.php/Joorgportal/".$line['fldhelplink']."'>Mediawiki-Hilfe</a><br>";
-//  } else {
-//    echo "<a class='btn btn-primary' href='http://localhost/tools/mediawiki/index.php/Hauptseite'>Mediawiki-Hilfe</a><br>";
-//  }  
   $datei="help.".$langshort.".html";
   if ( file_exists ( $pfad.$datei ) ) {
     $lines = file($pfad.$datei);
@@ -20,11 +12,13 @@ function hilfefunc($menu,$idwert) {
       echo $helpline;
     }
   } else {
-    $query="SELECT * FROM tbltranslate WHERE fldName='NOHELP' and fldLang='".$langshort."'";
+    $query="SELECT * FROM tbltranslate WHERE fldname='NOHELP' and fldlang='".$langshort."'";
     //echo $query."<br>";
-    $result = mysql_query($query) or die(mysql_error());
-    if ($line = mysql_fetch_array($result)) {
-      $hilfe=$line['fldBez'];
+//    $result = mysql_query($query) or die(mysql_error());
+    $result = db_query($query,"",$gdbtyp);
+//    if ($line = mysql_fetch_array($result)) {
+    if ($line = db_fetch($result,$gdbtyp)) {
+      $hilfe=$line['fldbez'];
     } else {
       $hilfe="Keine Hilfe vorhanden";
     }

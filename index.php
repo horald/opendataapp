d51e88c521cf135791cb8f7b3b001427c696afe6
horald <horst.meyer.koeln@gmail.com>
2014-12-22 20:14:15 +0100

    Undocumented Changes

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php
 echo "<a href='http://horald.github.io/androidportal/'>Spielplatzapp</a><br>";
 echo "<a href='start.php'>Opendataapp</a><br>";
+
+
+
+echo "<html>";
+echo "  <head>";
+echo "    <meta charset='utf-8'>";
+echo "    <meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no'>";
+echo "    <title>Start</title>";
+
+echo "    <!-- Bootstrap -->";
+echo "    <link href='includes/bootstrap/css/bootstrap.min.css' rel='stylesheet'>";
+
+echo "  </head>";
+echo "  <body>";
+
+echo "  <a href='http://horald.github.io/androidportal/' class='btn btn-default btn-lg btn-block' role='button'>Spielplatzapp</a><br>";
+echo "  <a href='start.php' class='btn btn-default btn-lg btn-block' role='button'>OpendataApp (in Bearbeitung)</a><br>";
+
+echo "  </body>";
+echo "</html>";
 ?>

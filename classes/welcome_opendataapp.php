7a3a9e7f8c5d711d6efae20786927029f31a8006
horald <horst.meyer.koeln@gmail.com>
2014-12-22 18:49:25 +0100

    Undocumented Changes

--- classes/welcome_opendataapp.php
+++ classes/welcome_opendataapp.php
@@ -10,10 +10,7 @@ echo "<table>";
 $version = file_get_contents("../version.txt");
 echo "<tr><td>Version</td><td>: ".$version."</td></tr>";
 echo "<tr><td>Datenbanktyp</td><td>: ".$gdbtyp."</td></tr>";
-<<<<<<< HEAD
 echo "<tr><td>URL unter</td><td>: <a href='http://community.codefor.de/spielplatzapp/index.php' target='_blank'>Opendataapp bei codefor.de</a></td></tr>";
-=======
->>>>>>> ed66af43bd554edbfcdc552f7149a6ae70380d87
 echo "<tr><td>erstellt f&uumlr</td><td>: <a href='http://codefor.de/koeln/index.html' target='_blank'>codeforgermany</a></td></tr>";
 echo "<tr><td>Sourcecode unter</td><td>: <a href='https://github.com/horald/opendataapp' target='_blank'>github:opendataapp</a></td></tr>";
 echo "</table>";
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
 echo "<a href='http://horald.github.io/androidportal/'>Spielplatzapp</a><br>";
-echo "<a href='index_neu.php'>Opendataapp</a><br>";
+echo "<a href='start.php'>Opendataapp</a><br>";
 ?>

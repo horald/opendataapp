1003a327a96352951be33bbf5288d601c49a8fdd
horald <horst.meyer.koeln@gmail.com>
2014-12-22 20:21:27 +0100

    Undocumented Changes

--- classes/googlemap.php
+++ classes/googlemap.php
@@ -6,102 +6,104 @@ include("dbtools.php");
 include("bootstrapfunc.php");
 include("../sites/views/wp_".$menu."/showtab.inc.php");
 
-echo "googlemap";
      echo "<html>";
      echo "<head>";
+     echo "<meta http-equiv='content-type' content='text/html; charset=utf-8'/>";
+     echo "<title>Google Map</title>";
+     echo "<style type='text/css'>";
+     echo " body { font: normal 10pt Helvetica, Arial; }";
+     echo " #map { width: 640px; height: 480px; border: 0px; padding: 0px; }";
+     echo "</style>";
+     echo "<script src='http://maps.google.com/maps/api/js?v=3&sensor=false' type='text/javascript'></script>";
+     echo "<script type='text/javascript'>";
 
+     echo "var blue_icon = new google.maps.MarkerImage('http://maps.google.com/mapfiles/ms/micons/blue.png',";
+     echo "        new google.maps.Size(32, 32), new google.maps.Point(0, 0),";
+     echo "        new google.maps.Point(16, 32)";
+     echo ");";
+     echo "var red_icon = new google.maps.MarkerImage('http://labs.google.com/ridefinder/images/mm_20_red.png',";
+     echo "        new google.maps.Size(32, 32), new google.maps.Point(0, 0),";
+     echo "        new google.maps.Point(16, 32)";
+     echo ");";
+     echo "var center = null;";
+     echo "var map = null;";
+     echo "var currentPopup;";
+     echo "var bounds = new google.maps.LatLngBounds();";
+
+     echo "function addMarker(lat, lng, info, complete) {";
+     echo "        var pt = new google.maps.LatLng(lat, lng);";
+     echo "        bounds.extend(pt);";
+     echo "        var marker = null;";
+     echo "        if( complete == 0 ){";
+     echo "               marker = new google.maps.Marker({";
+     echo "                        position: pt,";
+     echo "                        icon: blue_icon,";
+     echo "                        map: map";
+     echo "                });";
+     echo "       } else {";
+     echo "               marker = new google.maps.Marker({";
+     echo "                        position: pt,";
+     echo "                        icon: red_icon,";
+     echo "                        map: map";
+     echo "                });";
+     echo "       }";
+     echo "       var popup = new google.maps.InfoWindow({";
+     echo "                content: info,";
+     echo "                maxWidth: 300";
+     echo "       });";
+     echo "       google.maps.event.addListener(marker, 'click', function() {";
+     echo "                if (currentPopup != null) {";
+     echo "                        currentPopup.close();";
+     echo "                        currentPopup = null;";
+     echo "                }";
+     echo "                popup.open(map, marker);";
+     echo "                currentPopup = popup;";
+     echo "       });";
+     echo "       google.maps.event.addListener(popup, 'click', function() {";
+     echo "         popup.close();";
+     echo "       });";
+     echo "}";
+
+     echo "function initMap(parameterliste) {";
+     echo "  var i = 0;";
+
+//                     $query="SELECT * FROM tblorte WHERE fldaktiv='J'";
+//                     echo $query."<br>";
+//                     $qryres = db_query($query," SELECT-Error",$gdbtyp);
+//                     $row = db_fetch($qryres,$gdbtyp);
+
+//                     $xkoor=$row['fldxkoor'];
+//                     $ykoor=$row['fldykoor']; 
+
+     echo "  meinDS = parameterliste.split(';');";
+     echo "        map = new google.maps.Map(document.getElementById('map'), {";
+
+//                     echo "center: new google.maps.LatLng(".$xkoor.", ".$ykoor."),";
+     echo "          center: new google.maps.LatLng(50.93664488843189, 6.961641311645508),";
+
+     echo "          zoom: 11,";
+     echo "          mapTypeId: google.maps.MapTypeId.ROADMAP,";
+     echo "          mapTypeControl: false,";
+     echo "          mapTypeControlOptions: {";
+     echo "                 style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR";
+     echo "          },";
+     echo "          navigationControl: true,";
+     echo "          navigationControlOptions: {";
+     echo "                 style: google.maps.NavigationControlStyle.SMALL";
+     echo "          }";
+     echo "  });";
 ?>
-     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-     <title>Google Map</title>
-     <style type="text/css">
-     body { font: normal 10pt Helvetica, Arial; }
-     #map { width: 640px; height: 480px; border: 0px; padding: 0px; }
-     </style>
-     <script src="http://maps.google.com/maps/api/js?v=3&sensor=false" type="text/javascript"></script>
-     <script type="text/javascript">
-
-     var blue_icon = new google.maps.MarkerImage("http://maps.google.com/mapfiles/ms/micons/blue.png",
-             new google.maps.Size(32, 32), new google.maps.Point(0, 0),
-             new google.maps.Point(16, 32)
-     );
-     var red_icon = new google.maps.MarkerImage("http://labs.google.com/ridefinder/images/mm_20_red.png",
-             new google.maps.Size(32, 32), new google.maps.Point(0, 0),
-             new google.maps.Point(16, 32)
-     );
-     var center = null;
-     var map = null;
-     var currentPopup;
-     var bounds = new google.maps.LatLngBounds();
-
-     function addMarker(lat, lng, info, complete) {
-             var pt = new google.maps.LatLng(lat, lng);
-             bounds.extend(pt);
-             var marker = null;
-             if( complete == 0 ){
-                    marker = new google.maps.Marker({
-                             position: pt,
-                             icon: blue_icon,
-                             map: map
-                     });
-            } else {
-                    marker = new google.maps.Marker({
-                             position: pt,
-                             icon: red_icon,
-                             map: map
-                     });
-            }
-            var popup = new google.maps.InfoWindow({
-                     content: info,
-                     maxWidth: 300
-            });
-            google.maps.event.addListener(marker, "click", function() {
-                     if (currentPopup != null) {
-                             currentPopup.close();
-                             currentPopup = null;
-                     }
-                     popup.open(map, marker);
-                     currentPopup = popup;
-            });
-
-            google.maps.event.addListener(popup, "click", function() {
-              popup.close();            
-            });
-           
-     }
-
-     function initMap(parameterliste) {
-       var i = 0;     	
-
-       meinDS = parameterliste.split(";");
-             map = new google.maps.Map(document.getElementById("map"), {
+         for (i=0; i < meinDS.length ; i++ ) {
+           meinArray = meinDS[i].split(",");
+           addMarker(meinArray[0], meinArray[1],'<a href="'+meinArray[2]+'">'+meinArray[3]+'</a>','desc','complete');
+         }
 <?php
-                     $query="SELECT * FROM tblorte WHERE fldaktiv='J'";
-                     //echo $query."<br>";
-                     $qryres = db_query($query," SELECT-Error",$gdbtyp);
-                     $row = db_fetch($qryres,$gdbtyp);
 
-                     $xkoor=$row['fldxkoor'];
-                     $ykoor=$row['fldykoor']; 
-                     echo "center: new google.maps.LatLng(".$xkoor.", ".$ykoor."),";
-                     //echo "center: new google.maps.LatLng(50.93664488843189, 6.961641311645508),";
-?>
-                     zoom: 11,
-                     mapTypeId: google.maps.MapTypeId.ROADMAP,
-                     mapTypeControl: false,
-                     mapTypeControlOptions: {
-                            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR
-                     },
-                     navigationControl: true,
-                     navigationControlOptions: {
-                            style: google.maps.NavigationControlStyle.SMALL
-                    }
-            });
+     echo "}";
 
-     }
-
-     </script>
-<?php
+     echo "</script>";
      echo "</head>";
+     //echo "==>1k<br>";
      $dbselarr = $_SESSION['DBSELARR'];
      $count=sizeof($dbselarr);
      //echo $count."=count<br>";
@@ -110,6 +112,7 @@ echo "googlemap";
      $qryres = db_query($query," SELECT-Error",$gdbtyp);
      $ds="";
      for ( $x = 0; $x < $count; $x++ ) {
+//     while ($row = db_fetch($qryres,$gdbtyp)){
         $arrquery="SELECT * FROM ".$pararray['dbtable']." WHERE fldindex=".$dbselarr[$x];
         //echo $arrquery."<br>";
         $qryarr = db_query($arrquery," SELECT-Error",$gdbtyp);

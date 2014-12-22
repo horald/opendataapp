ed4e8f864ce3924329146ddf90df3cc69c65be8b
horald <horst.meyer.koeln@gmail.com>
2014-12-22 19:43:22 +0100

    Undocumented Changes

--- classes/googlemap.php
+++ classes/googlemap.php
@@ -1,3 +1,10 @@
 <?php
+session_start();
+$menu=$_GET[menu];
+include("../config.php");
+include("dbtools.php");
+include("bootstrapfunc.php");
+include("../sites/views/wp_".$menu."/showtab.inc.php");
+
 echo "googlemap";
 ?>
